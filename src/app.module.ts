e7ea84d658a04a03b96229751587608419b05ccd
GabrielySN <gabriely.nascimento17@escola.pr.gov.br>
2026-06-30 11:34:12 -0300

    Comit inicial

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'banco_de_dados_v4.sql',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+})
+export class AppModule {}
